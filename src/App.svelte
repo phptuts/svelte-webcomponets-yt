61a299e74079e2813a41f533114c58fe6a58e516
Noah Glaser <glaserpower@gmail.com>
2021-01-02 15:51:22 -0800

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script>
+	import { onMount } from "svelte";
+	export let count = 0;
+
+	function onClick() {
+		count = +count + 1;
+	}
+	onMount(() => {
+		console.log("can use on mount");
+	});
+</script>
+
+<style>
+	button {
+		width: 100px;
+		height: 100px;
+	}
+</style>
+
+<button on:click={onClick}> Count {count} </button>
+
+<svelte:options tag="count-btn" />
